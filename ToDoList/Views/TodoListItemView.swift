3c4c9d5bb65ace822be5ff77813bfcfefc37dc87
Thanh Long <thanhlongnguyen140499@gmail.com>
2024-09-02 16:35:36 +0700

    Lesson 12: Done Checkmarks – SwiftUI To Do List

--- ToDoList/Views/TodoListItemView.swift
+++ ToDoList/Views/TodoListItemView.swift
@@ -28,6 +28,8 @@ struct TodoListItemView: View {
                 viewModel.toggleIsDone(item: item)
             }, label: {
                 Image(systemName: item.isDone ? "checkmark.circle.fill" : "circle")
+                    .foregroundColor(.blue
+                    )
             })
         }
     }
